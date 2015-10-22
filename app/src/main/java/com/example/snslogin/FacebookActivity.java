6739e2e995b064a8c5fc2e1e593bf45992bf441d
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2015-10-23 00:25:36 +0900

    use facebook button

--- app/src/main/java/com/example/snslogin/FacebookActivity.java
+++ app/src/main/java/com/example/snslogin/FacebookActivity.java
@@ -1,16 +1,27 @@
 package com.example.snslogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class FacebookActivity extends AppCompatActivity {
+
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_facebook);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -27,6 +38,26 @@ public class FacebookActivity extends AppCompatActivity {
                 logout();
             }
         });
+
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton facebookButton = (LoginButton) findViewById(R.id.login_button);
+        facebookButton.setReadPermissions("public_profile");
+        facebookButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                ((TextView) findViewById(R.id.status)).setText("Status: logged in");
+            }
+
+            @Override
+            public void onCancel() {
+                ((TextView) findViewById(R.id.status)).setText("Status: canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                ((TextView) findViewById(R.id.status)).setText("Status: error");
+            }
+        });
     }
 
     @Override
@@ -37,6 +68,12 @@ public class FacebookActivity extends AppCompatActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void login() {
