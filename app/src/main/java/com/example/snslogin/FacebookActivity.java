106be720432abfbde5e589bd16c24f542823ce66
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2015-10-22 23:59:59 +0900

    add facebook activity

---
+++ app/src/main/java/com/example/snslogin/FacebookActivity.java
@@ -0,0 +1,49 @@
+package com.example.snslogin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class FacebookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void login() {
+        ((TextView) findViewById(R.id.status)).setText("Status: logging in");
+    }
+
+    public void logout() {
+        ((TextView) findViewById(R.id.status)).setText("Status: logged out");
+    }
+}
--- app/src/main/java/com/example/snslogin/MainActivity.java
+++ app/src/main/java/com/example/snslogin/MainActivity.java
@@ -18,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, GoogleActivity.class));
             }
         });
+
+        findViewById(R.id.facebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FacebookActivity.class));
+            }
+        });
     }
 }
